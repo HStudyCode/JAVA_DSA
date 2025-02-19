0ae07aeb6674809ead68d3425d4f9e58cc9c766f
HStudyCode <hstudycode@gmail.com>
2025-02-19 22:42:24 +0530

    java

---
+++ patterns/p1_solid_Rectangle.java
@@ -0,0 +1,13 @@
+package patterns;
+
+public class p1_solid_Rectangle {
+    public static void main(String[] args) {
+        for (int i=1; i<=5; i++){
+            for(int j=1; j<=10; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        } 
+    }
+    
+}
