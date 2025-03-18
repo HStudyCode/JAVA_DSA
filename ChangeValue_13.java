6623b4f859736da68416e32ae0efe1d4d590e12d
HStudyCode <hstudycode@gmail.com>
2025-03-18 21:49:18 +0530

    JAVA

---
+++ ChangeValue_13.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class ChangeValue_13 {
+    public static void main(String[] args) {
+        // create an array
+        int[] arr = {1, 2, 3, 4, 5};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+        
+
+    }
+
+    static void change(int[] nums){
+        nums[0] = 10; //If you make a change to the object via this ref variable, same object will be changed
+        
+    }
+}
